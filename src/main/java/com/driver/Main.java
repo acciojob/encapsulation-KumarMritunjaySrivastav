6cb286bf0e4e8f06a8caf5cb61951b4b5550ad12
kumar mritunjay <kumarmritunjay082003@gmail.com>
2023-06-10 17:26:27 +0530

    Encapsulation done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "Ankush";
+//        System.out.println(obj.name);
+        obj.setName("Ankush");
+        obj.getName();
+    }
 }
